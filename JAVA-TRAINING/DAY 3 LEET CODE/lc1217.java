ac4a9970830656b6062378acaab0dd0cfe4da7d3
Harikishanth R <harikishanth@gmail.com>
2026-04-16 09:54:45 +0530

    Day 3 commit

---
+++ JAVA-TRAINING/DAY 3 LEET CODE/lc1217.java
@@ -0,0 +1,17 @@
+public class lc1217 {
+    public int minCostToMoveChips(int[] position) {
+        int odd=0,even=0;
+        for(int i:position)
+        {
+            if(i%2==0)
+            {
+                even++;
+            }
+            else
+            {
+                odd++;
+            }
+        }
+        return Math.min(even,odd);
+    }
+}
---
+++ JAVA-TRAINING/DAY 3 LEET CODE/lc1512.java
@@ -0,0 +1,12 @@
+public class lc1512 {
+    public int numIdenticalPairs(int[] nums) {
+        int[] h=new int[111];
+        int c=0;
+        for(int i:nums)
+        {
+            c+=h[i];
+            h[i]++;
+        }
+        return c;
+    }
+}
